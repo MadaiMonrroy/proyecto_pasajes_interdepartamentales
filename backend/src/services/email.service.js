e9cc4750dbc80a6b9efe6f4da51801d111243b53
MadaiMonrroy <madaimonrroy2810@gmail.com>
2026-05-16 16:12:04 -0400

    Configurar Brevo SMTP

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,25 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: 'smtp-relay.brevo.com',
   port: 587,
   secure: false,
-  requireTLS: true,
-  family: 4,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_SMTP_KEY
   }
 });
 
 async function enviarCorreo(para, asunto, html, attachments = []) {
+
   return await transporter.sendMail({
-    from: `"Sistema de Pasajes" <${process.env.GMAIL_USER}>`,
+    from: `"Sistema de Pasajes" <${process.env.BREVO_USER}>`,
     to: para,
     subject: asunto,
     html,
     attachments
   });
+
 }
 
 module.exports = { enviarCorreo };
