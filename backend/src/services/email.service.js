f9de2dc928729fe6a5de79c33282e90d7b6a268d
MadaiMonrroy <madaimonrroy2810@gmail.com>
2026-05-16 22:57:13 -0400

    revisar bug

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
-
 async function enviarCorreo(para, asunto, html) {
+  console.log('BREVO_API_KEY existe:', !!process.env.BREVO_API_KEY);
+  console.log('BREVO_SENDER_EMAIL:', process.env.BREVO_SENDER_EMAIL);
   return await axios.post(
     'https://api.brevo.com/v3/smtp/email',
     {
